b91b66f64f10f7a793e14c2bb2ce3941e2cecb91
csaba <csaba.kucsera93@gmail.com>
2024-08-15 18:19:11 +0200

    Create maven

---
+++ src/main/java/kucsera/peasentMarket/PeasentMarketApplication.java
@@ -0,0 +1,13 @@
+package kucsera.peasentMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeasentMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeasentMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kucsera/peasentMarket/PeasentMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package kucsera.peasentMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeasentMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
